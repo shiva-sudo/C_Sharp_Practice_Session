0dfb5d8c6cc9f9911cc8911f7a164451469a1e05
Shivpoojan Rajak <52307455+shiva-sudo@users.noreply.github.com>
2022-08-21 18:47:51 +0530

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PracticeSession
+{
+    class studentDetails
+    {
+        //Method 
+        public void StudentAge()
+        {
+            // Local Variable
+            int age = 26;
+            age = age + 10;
+            Console.WriteLine("Student Age is : {0} ", age);
+        }
+        static void Main(string[] args)
+        {
+            //Creating Object
+            studentDetails Obj = new studentDetails();
+            Obj.StudentAge();
+            Console.ReadLine();
+        }
+    }
+}
